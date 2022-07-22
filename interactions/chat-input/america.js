5dd7494d6901e76006bb78aba7e9b4e0e0e00ad4
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2022-07-22 21:43:06 +0100

    feat: /america even more rarer america 🐾

--- interactions/chat-input/america.js
+++ interactions/chat-input/america.js
@@ -9,11 +9,12 @@ import { autoArray, choice } from "@magicalbunny31/awesome-utility-stuff";
 export default async (interaction, redis) => {
    // america
    const america = choice([
-      ...autoArray(969, () => ({ field: `america`,  content: `america`,                         emoji: `🇺🇸` })),
-      ...autoArray(10,  () => ({ field: `amerwica`, content: `amerwica~ nyaa~`,                 emoji: `🇺🇸🐱` })),
-      ...autoArray(10,  () => ({ field: `acirema`,  content: `acirema`,                         emoji: `🇺🇸` })),
-      ...autoArray(10,  () => ({ field: `flood`,    content: `there is no america, only flood`, emoji: `<:Flood:983391790348509194>` })),
-      {                          field: `rare`,     content: `super rare america™️`,             emoji: `🇺🇸` }
+      ...autoArray(9699, () => ({ field: `america`,  content: `america`,                         emoji: `🇺🇸` })),
+      ...autoArray(100,  () => ({ field: `amerwica`, content: `amerwica~ nyaa~`,                 emoji: `🇺🇸🐱` })),
+      ...autoArray(100,  () => ({ field: `acirema`,  content: `acirema`,                         emoji: `🇺🇸` })),
+      ...autoArray(100,  () => ({ field: `flood`,    content: `there is no america, only flood`, emoji: `<:Flood:983391790348509194>` })),
+      ...autoArray(10,   () => ({ field: `rare`,     content: `super rare america™️`,             emoji: `🇺🇸` })),
+      {                           field: `rarer`,    content: `even more rarer america™️™️`,       emoji: `🇺🇸` }
    ]);
 
 
