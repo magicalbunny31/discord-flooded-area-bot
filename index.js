904f9bd6284f61f1d88d8a5d6cb27b5c9bed9401
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2022-08-11 00:26:28 +0100

    fix: connect timeout to prevent error on reboot 🐾

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ import { createClient } from "redis";
 const redis = createClient({
    socket: {
       host: process.env.REDIS_HOST,
-      port: +process.env.REDIS_PORT
+      port: +process.env.REDIS_PORT,
+      connectTimeout: 300
    },
    password: process.env.REDIS_AUTH
 });
@@ -96,7 +97,7 @@ process.on("uncaughtException", async (error, origin) => {
    process.exit(1);
 });
 
-redis.on(`error`, async error => {
+redis.on(`error`, async error => { console.log(error.errno, error.code, error.syscall)
    await sendBotError(
       `redis`,
       {
