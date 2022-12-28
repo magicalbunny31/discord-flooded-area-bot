8e69e13a9f13efcd9789ac3e67c21ffb2e49529f
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2022-12-28 02:03:13 +0000

    feat: /get-part-info 🐾

--- interactions/autocomplete/get-part-info.js
+++ interactions/autocomplete/get-part-info.js
@@ -11,169 +11,13 @@ export default async (interaction, firestore) => {
 
 
    // part names/ids
-   const parts = [{
-      name: `🎈 balloon`,
-      value: `Balloon`
-   }, {
-      name: `🎈 big balloon`,
-      value: `Big_Balloon`
-   }, {
-      name: `💣 big explosive`,
-      value: `Big_Explosive`
-   }, {
-      name: `🖥️ big gaming computer`,
-      value: `Big_Gaming_Computer`
-   }, {
-      name: `🍞 bread`,
-      value: `Bread`
-   }, {
-      name: `🔘 button`,
-      value: `Button`
-   }, {
-      name: `🔨 crafting table`,
-      value: `Crafting_Table`
-   }, {
-      name: `💣 explosive`,
-      value: `Explosive`
-   }, {
-      name: `💥 explosive shock`,
-      value: `Explosive_Shock`
-   }, {
-      name: `🛏️ fabric`,
-      value: `Fabric`
-   }, {
-      name: `🖥️ gaming computer`,
-      value: `Gaming_Computer`
-   }, {
-      name: `🪟 glass`,
-      value: `Glass`
-   }, {
-      name: `🔦 laser`,
-      value: `Lazer`
-   }, {
-      name: `🍃 leaves`,
-      value: `Leaves`
-   }, {
-      name: `💡 light (normal)`,
-      value: `Light`
-   }, {
-      name: `💡 red light`,
-      value: `Red_Light`
-   }, {
-      name: `💡 green light`,
-      value: `Green_Light`
-   }, {
-      name: `💡 blue light`,
-      value: `Blue_Light`
-   }, {
-      name: `💡 yellow light`,
-      value: `Yellow_Light`
-   }, {
-      name: `💡 cyan light`,
-      value: `Cyan_Light`
-   }, {
-      name: `💡 pink light`,
-      value: `Pink_Light`
-   }, {
-      name: `🚧 metal`,
-      value: `Metal`
-   }, {
-      name: `🪢 rope bundle`,
-      value: `Rope_Bundle`
-   }, {
-      name: `📺 screen`,
-      value: `Screen`
-   }, {
-      name: `🪨 stone`,
-      value: `Stone`
-   }, {
-      name: `🚀 thruster`,
-      value: `Thruster`
-   }, {
-      name: `💡 wired light`,
-      value: `Wired_Light`
-   }, {
-      name: `🪵 wood`,
-      value: `Wood`
-   }, {
-      name: `🛢️ oil`,
-      value: `Oil`
-   }, {
-      name: `🫘 beans`,
-      value: `Beans`
-   }, {
-      name: `🍪 cookie`,
-      value: `Cookie`
-   }, {
-      name: `🎶 music player`,
-      value: `Music_Player`
-   }, {
-      name: `🎶 rusty music player (broken)`,
-      value: `Rusty_Music_Player`
-   }, {
-      name: `⬜ plastic`,
-      value: `Plastic`
-   }, {
-      name: `🧀 cheese`,
-      value: `Cheese`
-   }, {
-      name: `💧 air tank`,
-      value: `Air_Tank`
-   }, {
-      name: `📦 container`,
-      value: `Container`
-   }, {
-      name: `🌾 farming plot`,
-      value: `Farming_Plot`
-   }, {
-      name: `💰 money bag`,
-      value: `Money_Bag`
-   }, {
-      name: `🏭 oil pump`,
-      value: `Oil_Pump`
-   }, {
-      name: `🪜 wooden truss`,
-      value: `Wooden_Truss`
-   }, {
-      name: `🎁 present (1)`,
-      value: `Present_1`
-   }, {
-      name: `🎁 present (2)`,
-      value: `Present_2`
-   }, {
-      name: `🎁 present (3)`,
-      value: `Present_3`
-   }, {
-      name: `🕳️ grate`,
-      value: `Grator`
-   }, {
-      name: `🏗️ corroded metal`,
-      value: `CorrodedMetal`
-   }, {
-      name: `👚 fabric wheel`,
-      value: `Fabric_Wheel`
-   }, {
-      name: `🛞 motor`,
-      value: `Motor`
-   }, {
-      name: `💺 seat`,
-      value: `Seat`
-   }, {
-      name: `🔒 locked seat`,
-      value: `Locked_Seat`
-   }, {
-      name: `🏴 american flag`,
-      value: `American_Flag`
-   }, {
-      name: `🍰 cake`,
-      value: `Cake`
-   }, {
-      name: `💨 whoopee cushion (broken)`,
-      value: `Whoopee_Cushion`
-   }, {
-      name: `⚡ junction`,
-      value: `Junction`
-   }]
+   const parts = Object.entries((await firestore.collection(`command`).doc(`get-part-info`).get()).data())
+      .map(([ id, partInfo ]) =>
+         ({
+            name: partInfo.name,
+            value: id
+         })
+      )
       .sort((a, b) => a.value.localeCompare(b.value));
 
 
@@ -186,9 +30,9 @@ export default async (interaction, firestore) => {
 
    // sort parts based on input
    const sortedParts = findSimilar(input, parts, {
-      key: `value`,
+      key: `name`,
       limit: 25,
-      minScore: 0.2
+      minScore: 0.1
    });
 
 
--- interactions/chat-input/get-part-info.js
+++ interactions/chat-input/get-part-info.js
@@ -3,16 +3,16 @@ export const data = new Discord.SlashCommandBuilder()
    .setDescription(`🏷️ get some info on a part`)
    .addStringOption(
       new Discord.SlashCommandStringOption()
-         .setName(`part-name`)
-         .setDescription(`🧱 this part's name`)
+         .setName(`part`)
+         .setDescription(`🧱 part to get info for`)
          .setAutocomplete(true)
          .setRequired(true)
    );
 
-export const guildOnly = true;
-
 
 import Discord from "discord.js";
+import fs from "fs/promises";
+
 import { colours, emojis, strip } from "@magicalbunny31/awesome-utility-stuff";
 
 /**
@@ -21,183 +21,25 @@ import { colours, emojis, strip } from "@magicalbunny31/awesome-utility-stuff";
  */
 export default async (interaction, firestore) => {
    // options
-   const partName = interaction.options.getString(`part-name`);
+   const partId = interaction.options.getString(`part`);
 
 
-   // part names/ids
-   const parts = [{
-      name: `🎈 balloon`,
-      value: `Balloon`
-   }, {
-      name: `🎈 big balloon`,
-      value: `Big_Balloon`
-   }, {
-      name: `💣 big explosive`,
-      value: `Big_Explosive`
-   }, {
-      name: `🖥️ big gaming computer`,
-      value: `Big_Gaming_Computer`
-   }, {
-      name: `🍞 bread`,
-      value: `Bread`
-   }, {
-      name: `🔘 button`,
-      value: `Button`
-   }, {
-      name: `🔨 crafting table`,
-      value: `Crafting_Table`
-   }, {
-      name: `💣 explosive`,
-      value: `Explosive`
-   }, {
-      name: `💥 explosive shock`,
-      value: `Explosive_Shock`
-   }, {
-      name: `🛏️ fabric`,
-      value: `Fabric`
-   }, {
-      name: `🖥️ gaming computer`,
-      value: `Gaming_Computer`
-   }, {
-      name: `🪟 glass`,
-      value: `Glass`
-   }, {
-      name: `🔦 laser`,
-      value: `Lazer`
-   }, {
-      name: `🍃 leaves`,
-      value: `Leaves`
-   }, {
-      name: `💡 light (normal)`,
-      value: `Light`
-   }, {
-      name: `💡 red light`,
-      value: `Red_Light`
-   }, {
-      name: `💡 green light`,
-      value: `Green_Light`
-   }, {
-      name: `💡 blue light`,
-      value: `Blue_Light`
-   }, {
-      name: `💡 yellow light`,
-      value: `Yellow_Light`
-   }, {
-      name: `💡 cyan light`,
-      value: `Cyan_Light`
-   }, {
-      name: `💡 pink light`,
-      value: `Pink_Light`
-   }, {
-      name: `🚧 metal`,
-      value: `Metal`
-   }, {
-      name: `🪢 rope bundle`,
-      value: `Rope_Bundle`
-   }, {
-      name: `📺 screen`,
-      value: `Screen`
-   }, {
-      name: `🪨 stone`,
-      value: `Stone`
-   }, {
-      name: `🚀 thruster`,
-      value: `Thruster`
-   }, {
-      name: `💡 wired light`,
-      value: `Wired_Light`
-   }, {
-      name: `🪵 wood`,
-      value: `Wood`
-   }, {
-      name: `🛢️ oil`,
-      value: `Oil`
-   }, {
-      name: `🫘 beans`,
-      value: `Beans`
-   }, {
-      name: `🍪 cookie`,
-      value: `Cookie`
-   }, {
-      name: `🎶 music player`,
-      value: `Music_Player`
-   }, {
-      name: `🎶 rusty music player (broken)`,
-      value: `Rusty_Music_Player`
-   }, {
-      name: `⬜ plastic`,
-      value: `Plastic`
-   }, {
-      name: `🧀 cheese`,
-      value: `Cheese`
-   }, {
-      name: `💧 air tank`,
-      value: `Air_Tank`
-   }, {
-      name: `📦 container`,
-      value: `Container`
-   }, {
-      name: `🌾 farming plot`,
-      value: `Farming_Plot`
-   }, {
-      name: `💰 money bag`,
-      value: `Money_Bag`
-   }, {
-      name: `🏭 oil pump`,
-      value: `Oil_Pump`
-   }, {
-      name: `🪜 wooden truss`,
-      value: `Wooden_Truss`
-   }, {
-      name: `🎁 present (1)`,
-      value: `Present_1`
-   }, {
-      name: `🎁 present (2)`,
-      value: `Present_2`
-   }, {
-      name: `🎁 present (3)`,
-      value: `Present_3`
-   }, {
-      name: `🕳️ grate`,
-      value: `Grator`
-   }, {
-      name: `🏗️ corroded metal`,
-      value: `CorrodedMetal`
-   }, {
-      name: `👚 fabric wheel`,
-      value: `Fabric_Wheel`
-   }, {
-      name: `🛞 motor`,
-      value: `Motor`
-   }, {
-      name: `💺 seat`,
-      value: `Seat`
-   }, {
-      name: `🔒 locked seat`,
-      value: `Locked_Seat`
-   }, {
-      name: `🏴 american flag`,
-      value: `American_Flag`
-   }, {
-      name: `🍰 cake`,
-      value: `Cake`
-   }, {
-      name: `💨 whoopee cushion (broken)`,
-      value: `Whoopee_Cushion`
-   }, {
-      name: `⚡ junction`,
-      value: `Junction`
-   }];
+   // defer the interaction
+   await interaction.deferReply();
+
+
+   // parts
+   const parts = (await firestore.collection(`command`).doc(`get-part-info`).get()).data();
+   const part = parts[partId];
 
 
    // this option isn't in the parts array
-   if (!parts.map(part => part.value).includes(partName))
-      return await interaction.reply({
+   if (!part)
+      return await interaction.editReply({
          content: strip`
-            ❌ **\`${partName}\` isn't a part!**
+            ❌ **\`${partId}\` isn't a part!**
             > try selecting from the inline autocomplete choices for parts ${emojis.happ}
-         `,
-         ephemeral: true
+         `
       });
 
 
@@ -206,25 +48,50 @@ export default async (interaction, firestore) => {
       new Discord.EmbedBuilder()
          .setColor(colours.flooded_area)
          .setAuthor({
-            name: parts.find(part => part.value === partName).name,
-            iconURL: null // TODO
+            name: part.name
          })
-         .setDescription(strip`
-            🧱 **part id (for endless; case sensitive)**
-            > \`${parts.find(part => part.value === partName).value}\`
-         `)
-         .setFooter({
-            text: strip`
-               📋 more info to this command is coming soon!!
-               🐰 bunny is lazy and wants to take a break now
-            `
+         .setThumbnail(`attachment://${partId}.png`)
+         .setDescription(`*${part[`flavour-text`]}*`)
+         .addFields({
+            name: `❓ what is it?`,
+            value: `>>> ${part.description}`,
+            inline: false
+         }, {
+            name: `${
+               part[`obtained-through`] === `finding it out in the open`
+                  ? `⬛`
+                  : part[`obtained-through`] === `crafting or part packs`
+                     ? `🟥`
+                     : `🟨`
+            } obtained through..`,
+            value: part[`obtained-through`],
+            inline: true
+         }, {
+            name: `⚒️ crafting recipe`,
+            value: part[`crafting-recipe`]
+               .map(({ quantity, part }) => `\`${quantity}\` ${part}`)
+               .join(`\n`)
+               || `\u200b`,
+            inline: true
+         }, {
+            name: `🆔 id (case-sensitive)`,
+            value: `\`${partId}\``,
+            inline: true
          })
    ];
 
 
-   // reply to the interaction
-   return await interaction.reply({
+   // edit the deferred interaction
+   return await interaction.editReply({
       embeds,
-      ephemeral: true
+
+      files: (await fs.readdir(`./assets/parts`)).find(file => file === `${partId}.png`)
+         ? [
+            new Discord.AttachmentBuilder()
+               .setFile(`./assets/parts/${partId}.png`)
+               .setName(`${partId}.png`)
+               .setDescription(partId)
+         ]
+         : []
    });
 };
