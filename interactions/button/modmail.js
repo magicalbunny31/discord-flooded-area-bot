4f939b609792a9da0400cded93b7ac41839f6088
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2023-10-01 04:29:40 +0100

    ref: improve description 🐾

--- interactions/button/modmail.js
+++ interactions/button/modmail.js
@@ -26,24 +26,24 @@ export default async (interaction, firestore) => {
          .setTitle(`📬 Modmail Submissions`)
          .setDescription(strip`
             ### ${emojis.bun_paw_wave} ${choice([ `Hello`, `Hi`, `Welcome` ])}, ${interaction.user}!
-            > - Any member can submit ${interaction.channel} to send a message, or server-related question to the ${Discord.roleMention(process.env.FA_ROLE_HEAD_OF_MODERATION)}.
+            > - Anyone can submit ${interaction.channel} to send a message, or server-related question to the ${Discord.roleMention(process.env.FA_ROLE_HEAD_OF_MODERATION)}.
 
             ### ✅ You can submit modmail for...
             > - A message to the ${Discord.roleMention(process.env.FA_ROLE_HEAD_OF_MODERATION)}
             > - A server-related query or question
-            > - Claiming a ${Discord.channelMention(process.env.FA_CHANNEL_GIVEAWAYS)}
-            > - Concerns about another person here (underage, being weird on a consistent basis)
+            > - Claiming a prize in ${Discord.channelMention(process.env.FA_CHANNEL_GIVEAWAYS)}
+            > - Reporting a person where we would be able to look into their behaviour/the situation more in depth
             > - Issues with moderation in this server
             > - Help on something about this server
 
             ### ❌ You cannot submit modmail for...
-            > - Reporting players in ${Discord.hyperlink(`Flooded Area`, `https://www.roblox.com/games/3976767347/Flooded-Area`)} use ${Discord.channelMention(process.env.FA_CHANNEL_REPORT_A_PLAYER)}
+            > - Reporting a person actively causing harm in chat, ping the ${Discord.roleMention(process.env.FA_ROLE_MODERATION_TEAM)}
+            > - Reporting players in ${Discord.hyperlink(`Flooded Area`, `https://www.roblox.com/games/3976767347/Flooded-Area`)}, use ${Discord.channelMention(process.env.FA_CHANNEL_REPORT_A_PLAYER)}
             > - Appealing against moderative actions in ${Discord.hyperlink(`Flooded Area`, `https://www.roblox.com/games/3976767347/Flooded-Area`)}, use ${Discord.channelMention(process.env.FA_CHANNEL_BAN_APPEALS)}
             > - Reporting bugs in ${Discord.hyperlink(`Flooded Area`, `https://www.roblox.com/games/3976767347/Flooded-Area`)}, use ${Discord.channelMention(process.env.FA_CHANNEL_BUG_REPORTS)}
-            > - Reporting a member in this server for breaking the ${Discord.channelMention(process.env.FA_CHANNEL_RULES_AND_INFO)}, notify a member of the ${Discord.roleMention(process.env.FA_ROLE_MODERATION_TEAM)} instead
             > - Sending silly messages for no reason
 
-            ### 🚨 If there is an active situation in chat (such as a raid or racism), ping the ${Discord.roleMention(process.env.FA_ROLE_MODERATION_TEAM)} first
+            ### 🚨 Ping the ${Discord.roleMention(process.env.FA_ROLE_MODERATION_TEAM)} instead if there's an active situation in chat (like a raid or someone being racist)
             > - Do not bring up personal issues in ${interaction.channel} messages if it's not relevant.
          `)
          .setFooter({
