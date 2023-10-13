0d3a62289c26dc2eea303c60c2b998e654796f29
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2023-10-14 00:49:28 +0100

    feat: /say 🐾

---
+++ interactions/chat-input/say.js
@@ -0,0 +1,121 @@
+export const name = "say";
+export const guilds = [ process.env.GUILD_FLOODED_AREA ];
+
+export const data = new Discord.SlashCommandBuilder()
+   .setName(`say`)
+   .setDescription(`🐰`)
+   .addStringOption(
+      new Discord.SlashCommandStringOption()
+         .setName(`message`)
+         .setDescription(`🐇`)
+         .setMaxLength(2000)
+         .setRequired(false)
+   )
+   .addAttachmentOption(
+      new Discord.SlashCommandAttachmentOption()
+         .setName(`attachment`)
+         .setDescription(`🐇`)
+         .setRequired(false)
+   )
+   .addStringOption(
+      new Discord.SlashCommandStringOption()
+         .setName(`reply-to-message-id`)
+         .setDescription(`🐇`)
+         .setMinLength(17)
+         .setMaxLength(19)
+         .setRequired(false)
+   );
+
+
+import Discord from "discord.js";
+import { strip } from "@magicalbunny31/awesome-utility-stuff";
+
+/**
+ * @param {Discord.ChatInputCommandInteraction} interaction
+ * @param {import("@google-cloud/firestore").Firestore} firestore
+ */
+export default async (interaction, firestore) => {
+   // options
+   const content    = interaction.options.getString(`message`);
+   const attachment = interaction.options.getAttachment(`attachment`);
+   const replyId    = interaction.options.getString(`reply-to-message-id`);
+
+
+   // users
+   const developers = JSON.parse(process.env.DEVELOPERS.replaceAll(`'`, `"`));
+
+   const magicalbunny31 = developers[0];
+   const halo           = process.env.USER_HALO;
+   const mimi           = process.env.USER_MIMI;
+   const yourstruly     = process.env.USER_YOURSTRULY;
+
+
+   // this user can't use this command
+   const allowedUsers = [ magicalbunny31, halo, mimi, yourstruly ];
+
+   if (!allowedUsers.includes(interaction.user.id))
+      return await interaction.reply({
+         content: `🐰`,
+         ephemeral: true
+      });
+
+
+   // empty message
+   if (!content && !attachment)
+      return await interaction.reply({
+         content: strip`
+            ### ❌ Invalid arguments
+            > - At least one of \`message\` or \`attachment\` options are required.
+         `,
+         ephemeral: true
+      });
+
+
+   // attachment exceeds upload limit
+   const fileUploadLimit = (() => {
+      switch (interaction.guild.premiumTier) {
+         default:                             return 2.5e+7;
+         case Discord.GuildPremiumTier.Tier2: return 5e+7;
+         case Discord.GuildPremiumTier.Tier3: return 1e+8;
+      };
+   })();
+
+   if (attachment?.size > fileUploadLimit)
+      return await interaction.reply({
+         content: strip`
+            ### ❌ Invalid argument
+            > - \`attachment\` option file size exceeds this server's file upload limit.
+         `,
+         ephemeral: true
+      });
+
+
+   // reply to the interaction
+   await interaction.deferReply({
+      ephemeral: true
+   });
+
+
+   // send typing to the channel
+   await interaction.channel.sendTyping();
+
+
+   // send the message to this channel
+   await interaction.channel.send({
+      content,
+      files: attachment
+         ? [ attachment ]
+         : [],
+      reply: {
+         messageReference: replyId,
+         failIfNotExists: false
+      },
+      allowedMentions: {
+         parse: []
+      }
+   });
+
+
+   // delete the interaction's reply
+   return await interaction.deleteReply();
+};
